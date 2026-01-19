7c429a578866f7052718b97a17894407ac94eb5a
Ali-Ibrahim <ali.ibrahim.jasim@gmail.com>
2026-01-19 13:17:59 +0300

    add if-statement functionality to prompt for name input

--- exercise.py
+++ exercise.py
@@ -22,5 +22,5 @@ y = 4
 z = 5
 
 result = round(x)
-
+ 
 print(result)
---
+++ if-statement.py
@@ -0,0 +1,30 @@
+# if = Do some code only IF some condition is true
+#      Else = Do some code if the condition is NOT true
+
+#age = int(input('Enter your age: '))
+
+#if age >= 100:
+ #   print('You are too old to sign up!')
+#elif age >= 18:
+ #   print('You are now signed up!')
+#elif age < 0:
+ #   print('You havent been born yet!')
+#else:
+ #   print('You must be at least 18+ to sign up.')
+
+
+#response = input('Would you like food? (Y/N): ')
+
+#if response == 'Y':
+  #  print('Have some food!')
+#else:
+ #   print('No food for you!')
+name = input('Enter your name: ')
+
+if name == '':
+    print('You did not enter a name!')
+else :
+    print(f'Hello {name}!')
+
+
+
